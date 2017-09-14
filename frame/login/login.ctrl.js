6ed4babf2f98daaff9c389302322d2e2707ccc59
caoruixia <1361874583@qq.com>
2017-09-14 15:18:07 +0800

    add caoruixia 登录 20170914

--- frame/login/login.ctrl.js
+++ frame/login/login.ctrl.js
@@ -2,13 +2,28 @@ define(['app'], function (app) {
     'use strict';
     app.controller('LoginCtrl', ['$scope','$location','$stateParams','AuthHandler',
         function ($scope,$location,$stateParams,AuthHandler) {
-
-            $scope.loginmessage = $stateParams.msg;
+            var Pattern=/^(?![\d]+$)(?![a-zA-Z]+$)(?![^\da-zA-Z]+$).{6,20}$/  //能匹配的组合为：数字+字母，数字+特殊字符，字母+特殊字符，数字+字母+特殊字符组合，而且不能是纯数字，纯字母，纯特殊字符
+            // $scope.loginmessage = $stateParams.msg;
 
             //系统登录
             $scope.login = function(){
                 try{
-                    var authUser = $scope.auth.user;
+                    var authUser= {};
+                    authUser.usercode = $scope.usercode;
+                    authUser.password = $scope.password;
+                    if (authUser == '' || authUser == null) {
+                    }
+                    if (authUser.usercode == undefined) {
+                        $scope.loginmessage ="请输入您的用户名";
+                        return;
+                    }else if(authUser.password == undefined) {
+                        $scope.loginmessage ="请输入您的密码"
+                        return;
+                    }else if(!Pattern.test($scope.password)){
+                        $scope.loginmessage ="密码必须为6-20位英文字母、数字、下划线"
+                        // $scope.testCodeCheck=true;
+                        return;
+                    }
                     AuthHandler.login(authUser).then(
                         function(answer){
                             if(answer.status){
@@ -29,6 +44,10 @@ define(['app'], function (app) {
                     $scope.loginmessage = "系统异常,请联系管理员";
                 }
             };
+            //忘记密码
+            $scope.forgetPassword=function () {
+                $scope.loginmessage = "如您忘记密码，请联系管理员！";
+            }
         }]);
     //编写指令，用于页面repeatFinish渲染完成
 //    app.directive('repeatFinish', function () {
