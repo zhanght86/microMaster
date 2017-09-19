00d395e52dbbc22e866435f97e78c86fa56aae3f
caoruixia <1361874583@qq.com>
2017-09-19 13:19:03 +0800

    add caoruixia  20170919 区域管理(添加修改停用)

--- components/region/controller/region.ctrl.js
+++ components/region/controller/region.ctrl.js
@@ -5,8 +5,31 @@
 define([], function () {
     'use strict';
     function regionCtrlHandler($scope,$state) {
-        $scope.showMainNagiv = false;
-    };
+        // 添加关联机构
+        $scope.addRelation=function () {
+            $scope.layer_addRalation=true;
+        };
+        // 弹层关闭
+        $scope.addralation_sure=function () {
+            $scope.layer_addRalation=false;
+        };
+        // 修改关联机构
+        $scope.modify=function () {
+            $scope.layer_modRalation=true;
+        };
+        // 弹层关闭
+        $scope.modralation_sure=function () {
+            $scope.layer_modRalation=false;
+        };
+        //停用
+        $scope.Disable=function () {
+            $scope.layer_Disable=true;
+            $scope.tipMessage="您确定停用吗？";
+        };
+        $scope.rescind=function () {
+            $scope.layer_Disable=false;
+        };
 
+    };
     return regionCtrlHandler;
 });
