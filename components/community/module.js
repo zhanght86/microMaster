62cd9d413ada1518dee70c6653dcc13fc56a507b
lichenyi <lichenyi@sinosoft.com.cn>
2017-09-19 14:27:09 +0800

    index.css 从557line添加; \microMaster\components\material.js 修改; 添加:microMaster\components\community\tpl\servi.html,\microMaster\components\community\service\servi.serv.js,\microMaster\components\community\controller\servi.ctrl.js

---
+++ components/community/module.js
@@ -0,0 +1,28 @@
+/**
+ * Created by ZhangJiansen on 2016/9/18.
+ * 文件资料维护模块定义
+ */
+define([
+    'angular',
+    'components/community/controller/servi.ctrl',
+    'components/community/service/servi.serv',
+],function (angular,
+            serviCtrlHandler,
+            serviServHandler) {
+    'use strict';
+    console.log("load business.community module");
+    /*文件资料维护*/
+    return angular.module('business.community', [])
+        .config(['$stateProvider',
+            function ($stateProvider) {
+                $stateProvider
+                    .state("main.community", {
+                        url:"/community",
+                        templateUrl: "components/community/tpl/servi.html"
+                        //controller: ""
+                    });
+            }])
+        .controller('serviCtrl',serviCtrlHandler)
+        .service('serviServ',serviServHandler);
+});
+
