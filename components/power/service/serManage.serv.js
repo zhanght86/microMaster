16fa4fb0d150175c0a311f4cc64dad48e4df3f9a
caoruixia <1361874583@qq.com>
2017-09-19 09:51:56 +0800

    add caoruixia  20170919 权限管理

---
+++ components/power/service/serManage.serv.js
@@ -0,0 +1,16 @@
+/**
+ * Created by ZhangJiansen on 2016/9/11.
+ */
+define([], function () {
+    'use strict';
+    function serManageServHandler($http,$q){
+        return {
+
+
+
+
+        }
+    };
+
+    return serManageServHandler;
+});
