4e0748a539580d6e9a6ce9aa8ad69291fa6adb85
caoruixia <1361874583@qq.com>
2017-09-20 11:09:56 +0800

    add caoruixia  20170920  展业人员管理-普通人员管理(会员详细信息，保单详情，车商渠道详情)

--- components/suspend/controller/suspend.ctrl.js
+++ components/suspend/controller/suspend.ctrl.js
@@ -4,15 +4,13 @@
 define([], function () {
     'use strict';
     function suspendCtrlHandler($scope,$state,suspendServ) {
-        console.log("suspendCtrlHandler enter");
+
         /*查询被保人清单*/
         var getInsuredList = function(){
             var pageIndex = $scope.paginationConf.currentPage;
             var pageSize = $scope.paginationConf.itemsPerPage;
-
             suspendServ.ProqueryByPage(pageIndex,pageSize).then(
                 function(answer){
-                    //console.log(JSON.stringify(answer.data));
                     console.log("count="+answer.data.count);
                     $scope.paginationConf.totalItems = answer.data.count;
                     $scope.insureDatas = answer.data.items;
@@ -36,12 +34,12 @@ define([], function () {
             $scope.$watch('paginationConf.currentPage + paginationConf.itemsPerPage', getInsuredList);
 
             /!*tab切换响应方法*!/
-            //批改奖励调整
+            //普通用户管理
             $scope.applicationTab = function(){
                 $scope.curActive='appli';
                 $state.go("main.suspend.proposal");
             };
-            //批改结果查询
+            //展业人员管理
             $scope.endorseTab=function(){
                 $scope.curActive='endor';
                 $state.go("main.suspend.endorse");
@@ -53,7 +51,26 @@ define([], function () {
         };
         initFunc();
 
-    };
+        //账户名(会员详细信息)
+        $scope.userMess=function () {
+            $scope.layer_Policy=true;
+        };
+        //承包笔数(保单详情)
+        $scope.nums=function () {
+            $scope.layer_customer=true;
+        };
+        //人员属性(车商渠道详情)
+        $scope.attribute=function () {
+            $scope.layer_carChannel=true;
+        };
+        //关闭
+        $scope.addRole_back=function () {
+            $scope.layer_Policy=false;
+            $scope.layer_customer=false;
+            $scope.layer_carChannel=false;
+        };
 
+
+    };
     return suspendCtrlHandler;
 });
