6bfe6f2d9b6a107aa930975bc1ab73ad8301025f
caoruixia <1361874583@qq.com>
2017-09-26 13:52:46 +0800

    add caoruixia  20170926  首页

--- components/index/module.js
+++ components/index/module.js
@@ -5,26 +5,10 @@
 define([
     'angular',
     'components/index/controller/index.ctrl',
-    // 'components/index/controller/focusManage.ctrl',
-    // 'components/index/controller/regionManage.ctrl',
-    // 'components/index/controller/serManage.ctrl',
-    // 'components/index/controller/addRole.ctrl',
     'components/index/service/index.serv',
-    // 'components/index/service/focusManage.serv',
-    // 'components/index/service/regionManage.serv',
-    // 'components/index/service/serManage.serv',
-    // 'components/index/service/addRole.serv',
 ],function (angular,
             indexCtrlHandler,
-            // focusManageCtrlHandler,
-            // regionManageCtrlHandler,
-            // serManageCtrlHandler,
-            // addRoleCtrlHandler,
             indexServHandler){
-            // focusManageServHandler,
-            // regionManageServHandler,
-            // serManageServHandler,
-            // addRoleServHandler ) {
     'use strict';
     console.log("load business.index module");
     /*首页-统计图表*/
@@ -37,36 +21,8 @@ define([
                        templateUrl: "components/index/tpl/index.html",
                        controller: "indexCtrl"
                     });
-                // .state("main.focusManage", {
-                //         url:"/focusManage",
-                //         templateUrl: "components/index/tpl/focusManage.html",
-                //         controller: "focusManageCtrl"
-                // })
-                // .state("main.regionManage", {
-                //         url:"/regionManage",
-                //         templateUrl: "components/index/tpl/regionManage.html",
-                //         controller: "regionManageCtrl"
-                //  })
-                // .state("main.serManage", {
-                //         url:"/serManage",
-                //         templateUrl: "components/index/tpl/serManage.html",
-                //         controller: "serManageCtrl"
-                // })
-                // .state("main.addRole", {
-                //     url:"/addRole",
-                //     templateUrl: "components/index/tpl/addRole.html",
-                //     controller: "addRoleCtrl"
-                // });
             }])
         .controller('indexCtrl',indexCtrlHandler)
-        // .controller('focusManageCtrl',focusManageCtrlHandler)
-        // .controller('regionManageCtrl',regionManageCtrlHandler)
-        // .controller('serManageCtrl',serManageCtrlHandler)
-        // .controller('addRoleCtrl',addRoleCtrlHandler)
         .service('indexServ',indexServHandler);
-        // .service('focusManageServ',focusManageServHandler)
-        // .service('regionManageServ',regionManageServHandler)
-        // .service('serManageServ',serManageServHandler)
-        // .service('addRoleServ',addRoleServHandler);
 });
 
