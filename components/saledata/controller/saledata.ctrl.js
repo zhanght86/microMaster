b75e4f492abe59e7b9303c77c7975f7865ddfd21
caoruixia <1361874583@qq.com>
2017-09-26 16:44:20 +0800

    add caoruixia  20170926  销售数据管理-订单查询

--- components/saledata/controller/saledata.ctrl.js
+++ components/saledata/controller/saledata.ctrl.js
@@ -18,49 +18,10 @@ define([], function () {
             $scope.curActive='appli';
             $state.go("main.saledata.order");
 
-        //===================================普通用户管理部分=====================================
-        //账户名(会员详细信息)
-        $scope.userMess=function () {
-            $scope.layer_Policy=true;
-        };
-        //承包笔数(保单详情)
-        $scope.nums=function () {
-            $scope.layer_customer=true;
-        };
-        //人员属性(车商渠道详情)
-        $scope.attribute=function () {
-            $scope.layer_carChannel=true;
-        };
-        //===================================展业人员管理部分=====================================
-        //账户名(业务员详细信息)
-        $scope.enUserMess=function () {
-            $scope.layer_enPolicy=true;
-        };
-        //承包笔数(月，总)
-        $scope.enThisnum=function () {
-            $scope.layer_encustomer=true;
-        };
-        $scope.enTotalnum=function () {
-            $scope.layer_entotalcustomer=true;
-        };
-        //绩效(月，总)
-        $scope.enThisMoney=function () {
-            $scope.layer_performance=true;
-        };
-        $scope.enTotalMoney=function () {
-            $scope.layer_totalperformance=true;
-        };
-        //关闭
-        $scope.addRole_back=function () {
-            $scope.layer_Policy=false;
-            $scope.layer_customer=false;
-            $scope.layer_carChannel=false;
-            $scope.layer_enPolicy=false;
-            $scope.layer_encustomer=false;
-            $scope.layer_entotalcustomer=false;
-            $scope.layer_performance=false;
-            $scope.layer_totalperformance=false;
-        };
+        //===================================订单查询部分=====================================
+
+        //===================================保单查询部分=====================================
+
 
     };
     return saledataCtrlHandler;
