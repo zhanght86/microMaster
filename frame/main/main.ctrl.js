c2370159e7c6cd3a3615a454f0fe4ebdca267290
caoruixia <1361874583@qq.com>
2017-09-29 10:38:46 +0800

    add caoruixia  20170929   菜单栏效果添加

--- frame/main/main.ctrl.js
+++ frame/main/main.ctrl.js
@@ -21,16 +21,16 @@ define(['app', 'config','uiRouter'
                 //$location.path("/login?msg=注销成功");
             };
 
-            ////鼠标进入菜单区域显示菜单
-            //$scope.mouseenter = function () {
-            //    $scope.leftFlag = false;
-            //    $scope.rightFlag = false;
-            //}
-            ////鼠标离开菜单区域隐藏菜单
-            //$scope.mouseleave = function () {
-            //    $scope.leftFlag = true;
-            //    $scope.rightFlag = true;
-            //}
+            //鼠标进入菜单区域显示菜单
+            $scope.mouseenter = function () {
+               $scope.leftFlag = false;
+               $scope.rightFlag = false;
+            };
+            //鼠标离开菜单区域隐藏菜单
+            $scope.mouseleave = function () {
+               $scope.leftFlag = true;
+               $scope.rightFlag = true;
+            };
 
             //当前的日期
             $interval(function () {
