221b124642ac885f54be7ceb065b4deff06d23eb
caoruixia <1361874583@qq.com>
2017-10-20 15:27:49 +0800

    add caoruixia 20171020 客户经理修改

--- components/application/service/application.serv.js
+++ components/application/service/application.serv.js
@@ -5,7 +5,7 @@ define([], function () {
     'use strict';
     function applicationServHandler($http,$q){
         return {
-            /*投保单查询*/
+            /*..*/
             getData:function (params){
                 var deferred = $q.defer();
                 var urlGetData = "data/application.json"; //prpins-web/query
@@ -28,85 +28,7 @@ define([], function () {
                 //返回promise对象，交由Controller继续处理
                 return deferred.promise;
             },
-            /*投保单暂存*/
-            savePause:function (proposal){
-                console.log(proposal);
-                var urlSavePolicy = "data/application.json"; //prpins-web/save
-                var deferred = $q.defer();
-                var promise  = $http({
-                    //method:'POST',
-                    method:'POST',
-                    //url:urlSavePolicy,
-                    url:'/prpins-web/proposal/save',
-                    //data:policy
-                    data:proposal
-                });
-                promise.then(
-                    //通讯成功
-                    function(answer){
-                        answer.status = true;
-                        deferred.resolve(answer);
-                    },
-                    //通讯失败
-                    function(error){
-                        error.status = false;
-                        deferred.reject(error);
-                    });
-                //返回promise对象，交由Controller继续处理
-                return deferred.promise;
-            },
-            /*投保单提交*/
-            savePolicy:function (policy){
-                var urlSavePolicy = "data/application.json"; //prpins-web/save
-                var deferred = $q.defer();
-                var promise  = $http({
-                    //method:'POST',
-                    method:'GET',
-                    url:urlSavePolicy,
-                    //url:'/prpins-web/save',
-                    //data:policy
-                    data:policy
-                });
-                promise.then(
-                    //通讯成功
-                    function(answer){
-                        answer.status = true;
-                        deferred.resolve(answer);
-                    },
-                    //通讯失败
-                    function(error){
-                        error.status = false;
-                        deferred.reject(error);
-                    });
-                //返回promise对象，交由Controller继续处理
-                return deferred.promise;
-            },
-            /*投保单页面初始化*/
-            initProposal:function (policy){
-                console.log(policy);
-                var urlInit = "data/application.json"; //prpins-web/query
-                var deferred = $q.defer();
-                var promise  = $http({
-                    //method:'POST',
-                    method:'GET',
-                    url:urlInit,
-                    //url:'/prpins-web/proposal/initEdit',
-                    data:policy
-                });
-                promise.then(
-                    //通讯成功
-                    function(answer){
-                        answer.status = true;
-                        deferred.resolve(answer);
-                    },
-                    //通讯失败
-                    function(error){
-                        error.status = false;
-                        deferred.reject(error);
-                    });
-                //返回promise对象，交由Controller继续处理
-                return deferred.promise;
-            }
+
         }
     };
 
