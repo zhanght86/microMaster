1086c2cfbeb192d471dfbf8c49edd97e60ab96cf
caoruixia <1361874583@qq.com>
2017-10-20 13:08:34 +0800

    add caoruixia 20171020 权限管理-角色管理(查询，重置)

--- components/power/controller/focusManage.ctrl.js
+++ components/power/controller/focusManage.ctrl.js
@@ -4,8 +4,50 @@
  */
 define([], function () {
     'use strict';
-    function focusManageCtrlHandler($scope,$state) {
-        $scope.showMainNagiv = false;
+    function focusManageCtrlHandler($scope,$state,focusManageServ) {
+        //----------------------------查询------------------------------------------
+        var roleSearchDemand=function () {
+            $scope.condition.pageNum= $scope.paginationConf.currentPage;
+            $scope.condition.pageSize=$scope.paginationConf.itemsPerPage;
+            var conditionDto = $scope.condition;
+            if(conditionDto == '' || conditionDto == null){
+            }
+            if($scope.roleName == undefined){
+                $scope.roleName = "";
+            }
+            conditionDto.roleName=$scope.roleName;
+            focusManageServ.roleSearchserv(conditionDto).then(
+                function(answer){
+                    // $scope.paginationConf.totalItems = answer.data.data.total;
+                    // $scope.dataDictionaryListR=answer.data.data.list;
+                    $scope.selectRightRadio="";
+                },function(error){
+                    //cconsole.log(JSON.stringify(error.data));
+                }
+            );
+        };
+        var initPage = function(){
+            $scope.paginationConf = {
+                currentPage: 1,
+                totalItems: 0,
+                itemsPerPage: 5,
+                pagesLength: 5,
+                perPageOptions: [5, 10, 15,20]
+            };
+            $scope.condition = {
+                pageNum: $scope.paginationConf.currentPage,
+                pageSize: $scope.paginationConf.itemsPerPage
+            };
+            $scope.$watch('paginationConf.currentPage + paginationConf.itemsPerPage',roleSearchDemand);
+        };
+        initPage();
+        $scope.roleSearch=function () {
+            roleSearchDemand();
+        };
+        //----------------------------重置------------------------------------------
+        $scope.roleReset=function () {
+            $scope.roleName = "";
+        };
         //增加按钮跳转
         $scope.addRoleConfig=function () {
             $state.go("main.addRole");
