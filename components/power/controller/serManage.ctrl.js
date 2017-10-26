dd2cfcb36ff7b9bc20b745c9873c49500051d72a
caoruixia <1361874583@qq.com>
2017-10-26 14:31:27 +0800

    add caoruixia  20171026  人员配置(查询方法添加模拟数据展示)

--- components/power/controller/serManage.ctrl.js
+++ components/power/controller/serManage.ctrl.js
@@ -4,7 +4,51 @@
  */
 define([], function () {
     'use strict';
-    function serManageCtrlHandler($scope,$state) {
+    function serManageCtrlHandler($scope,$state,serManageServ) {
+        //----------------------------查询------------------------------------------
+        var rySearchDemand=function () {
+            $scope.condition.pageNum= $scope.paginationConf.currentPage;
+            $scope.condition.pageSize=$scope.paginationConf.itemsPerPage;
+            var conditionDto = $scope.condition;
+            if(conditionDto == '' || conditionDto == null){
+            }
+            if($scope.ygCode == undefined){
+                $scope.ygCode = "";
+            }
+            if($scope.ygName == undefined){
+                $scope.ygName = "";
+            }
+            conditionDto.ygCode=$scope.ygCode;
+            conditionDto.ygName=$scope.ygName;
+            serManageServ.ygSearchserv(conditionDto).then(
+                function(answer){
+                    // $scope.paginationConf.totalItems = answer.data.data.total;
+                    // $scope.dataDictionaryListR=answer.data.data.list;
+                    $scope.paginationConf.totalItems = answer.data.count;
+                    $scope.peopleDatalist=answer.data.items;
+                },function(error){
+                    //cconsole.log(JSON.stringify(error.data));
+                }
+            );
+        };
+        var initPage = function(){
+            $scope.paginationConf = {
+                currentPage: 1,
+                totalItems: 0,
+                itemsPerPage: 5,
+                pagesLength: 5,
+                perPageOptions: [5, 10, 15,20]
+            };
+            $scope.condition = {
+                pageNum: $scope.paginationConf.currentPage,
+                pageSize: $scope.paginationConf.itemsPerPage
+            };
+            $scope.$watch('paginationConf.currentPage + paginationConf.itemsPerPage',rySearchDemand);
+        };
+        initPage();
+        $scope.rySearch=function () {
+            rySearchDemand();
+        };
         //增加
         $scope.addRoleConfig=function () {
             $state.go("main.addStaf");
