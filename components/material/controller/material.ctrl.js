1fc3fe22413da28b22cf2960a4376625f4e1496a
caoruixia <1361874583@qq.com>
2017-10-26 17:39:06 +0800

    add caoruixia  20171026  日志管理

--- components/material/controller/material.ctrl.js
+++ components/material/controller/material.ctrl.js
@@ -5,6 +5,49 @@ define([], function () {
     function materialCtrl ($scope,$state,$stateParams,materialServ) {
 
 
+        //----------------------------查询------------------------------------------
+        var JournalSearchDemand=function () {
+            debugger
+            // $scope.condition.pageNum= $scope.paginationConf.currentPage;
+            // $scope.condition.pageSize=$scope.paginationConf.itemsPerPage;
+            var conditionDto = $scope.condition;
+            if(conditionDto == '' || conditionDto == null){
+            }
+            if($scope.Journal.describe == undefined){
+                $scope.Journal.describe = "";
+            }
+            conditionDto.describe=$scope.Journal.describe;
+            materialServ.materialServdata(conditionDto).then(
+                function(answer){
+                    // $scope.paginationConf.totalItems = answer.data.data.total;
+                    // $scope.dataDictionaryListR=answer.data.data.list;
+                    $scope.paginationConf.totalItems = answer.data.count;
+                    $scope.metList=answer.data.items;
+                },function(error){
+                    //cconsole.log(JSON.stringify(error.data));
+                }
+            );
+        };
+        // var initPages = function(){
+        //     $scope.paginationConf = {
+        //         currentPage: 1,
+        //         totalItems: 0,
+        //         itemsPerPage: 5,
+        //         pagesLength: 5,
+        //         perPageOptions: [5, 10, 15,20]
+        //     };
+        //     $scope.condition = {
+        //         pageNum: $scope.paginationConf.currentPage,
+        //         pageSize: $scope.paginationConf.itemsPerPage
+        //     };
+        //     $scope.$watch('paginationConf.currentPage + paginationConf.itemsPerPage',JournalSearchDemand);
+        // };
+        // initPages();
+        $scope.JournalSearch=function () {
+            debugger;
+            JournalSearchDemand();
+        };
+
 
     };
     return materialCtrl;
