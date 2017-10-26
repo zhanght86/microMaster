9ef30b4c112eaf6bbc0e20ffde66b418ed174c43
caoruixia <1361874583@qq.com>
2017-10-26 15:18:32 +0800

    add caoruixia  20171026  区域管理(查询方法添加模拟数据)

--- components/region/controller/region.ctrl.js
+++ components/region/controller/region.ctrl.js
@@ -5,6 +5,46 @@
 define([], function () {
     'use strict';
     function regionCtrlHandler($scope,$state,regionServ) {
+        //----------------------------查询------------------------------------------
+        var reSearchDemand=function () {
+            $scope.condition.pageNum= $scope.paginationConf.currentPage;
+            $scope.condition.pageSize=$scope.paginationConf.itemsPerPage;
+            var conditionDto = $scope.condition;
+            if(conditionDto == '' || conditionDto == null){
+            }
+            if($scope.XXX == undefined){
+                $scope.XXX = "";
+            }
+            conditionDto.XX=$scope.XXX;
+            regionServ.regionSearchserv(conditionDto).then(
+                function(answer){
+                    // $scope.paginationConf.totalItems = answer.data.data.total;
+                    // $scope.dataDictionaryListR=answer.data.data.list;
+                    $scope.paginationConf.totalItems = answer.data.count;
+                    $scope.regionDatalist=answer.data.items;
+                },function(error){
+                    //cconsole.log(JSON.stringify(error.data));
+                }
+            );
+        };
+        var initPage = function(){
+            $scope.paginationConf = {
+                currentPage: 1,
+                totalItems: 0,
+                itemsPerPage: 5,
+                pagesLength: 5,
+                perPageOptions: [5, 10, 15,20]
+            };
+            $scope.condition = {
+                pageNum: $scope.paginationConf.currentPage,
+                pageSize: $scope.paginationConf.itemsPerPage
+            };
+            $scope.$watch('paginationConf.currentPage + paginationConf.itemsPerPage',reSearchDemand);
+        };
+        initPage();
+        $scope.reSearch=function () {
+            reSearchDemand();
+        };
         // 添加关联机构
         $scope.addRelation=function () {
             $scope.layer_addRalation=true;
--- components/region/module.js
+++ components/region/module.js
@@ -5,26 +5,10 @@
 define([
     'angular',
     'components/region/controller/region.ctrl',
-    // 'components/index/controller/focusManage.ctrl',
-    // 'components/index/controller/regionManage.ctrl',
-    // 'components/index/controller/serManage.ctrl',
-    // 'components/index/controller/addRole.ctrl',
     'components/region/service/region.serv',
-    // 'components/index/service/focusManage.serv',
-    // 'components/index/service/regionManage.serv',
-    // 'components/index/service/serManage.serv',
-    // 'components/index/service/addRole.serv',
 ],function (angular,
             regionCtrlHandler,
-            // focusManageCtrlHandler,
-            // regionManageCtrlHandler,
-            // serManageCtrlHandler,
-            // addRoleCtrlHandler,
             regionServHandler){
-            // focusManageServHandler,
-            // regionManageServHandler,
-            // serManageServHandler,
-            // addRoleServHandler ) {
     'use strict';
     console.log("load business.region module");
     /*首页-统计图表*/
@@ -37,36 +21,8 @@ define([
                        templateUrl: "components/region/tpl/region.html",
                        controller: "regionCtrl"
                     });
-                // .state("main.focusManage", {
-                //         url:"/focusManage",
-                //         templateUrl: "components/index/tpl/focusManage.html",
-                //         controller: "focusManageCtrl"
-                // })
-                // .state("main.regionManage", {
-                //         url:"/regionManage",
-                //         templateUrl: "components/index/tpl/regionManage.html",
-                //         controller: "regionManageCtrl"
-                //  })
-                // .state("main.serManage", {
-                //         url:"/serManage",
-                //         templateUrl: "components/index/tpl/serManage.html",
-                //         controller: "serManageCtrl"
-                // })
-                // .state("main.addRole", {
-                //     url:"/addRole",
-                //     templateUrl: "components/index/tpl/addRole.html",
-                //     controller: "addRoleCtrl"
-                // });
             }])
         .controller('regionCtrl',regionCtrlHandler)
-        // .controller('focusManageCtrl',focusManageCtrlHandler)
-        // .controller('regionManageCtrl',regionManageCtrlHandler)
-        // .controller('serManageCtrl',serManageCtrlHandler)
-        // .controller('addRoleCtrl',addRoleCtrlHandler)
         .service('regionServ',regionServHandler);
-        // .service('focusManageServ',focusManageServHandler)
-        // .service('regionManageServ',regionManageServHandler)
-        // .service('serManageServ',serManageServHandler)
-        // .service('addRoleServ',addRoleServHandler);
 });
 
