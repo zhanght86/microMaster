f527d0d31f28d35fdfeefaf35d925b537a727afa
caoruixia <1361874583@qq.com>
2017-10-27 10:25:38 +0800

    add caoruixia  20171027  销售数据管理(保单查询添加模拟数据查询和分页功能)

--- components/saledata/controller/saledata.ctrl.js
+++ components/saledata/controller/saledata.ctrl.js
@@ -20,6 +20,45 @@ define([], function () {
         //===================================订单查询部分=====================================
 
         //===================================保单查询部分=====================================
+        var policySearchDemand=function () {
+            $scope.condition.pageNum= $scope.bdpaginationConf.currentPage;
+            $scope.condition.pageSize=$scope.bdpaginationConf.itemsPerPage;
+            var conditionDto = $scope.condition;
+            if(conditionDto == '' || conditionDto == null){
+            }
+            if($scope.roleName == undefined){
+                $scope.roleName = "";
+            }
+            conditionDto.roleName=$scope.roleName;
+            saledataServ.policySearchserv(conditionDto).then(
+                function(answer){
+                    // $scope.bdpaginationConf.totalItems = answer.data.data.total;
+                    // $scope.dataDictionaryListR=answer.data.data.list;
+                    $scope.bdpaginationConf.totalItems = answer.data.count;
+                    $scope.policyList=answer.data.items;
+                },function(error){
+                    //cconsole.log(JSON.stringify(error.data));
+                }
+            );
+        };
+        var initPage = function(){
+            $scope.bdpaginationConf = {
+                currentPage: 1,
+                totalItems: 0,
+                itemsPerPage: 5,
+                pagesLength: 5,
+                perPageOptions: [5, 10, 15,20]
+            };
+            $scope.condition = {
+                pageNum: $scope.bdpaginationConf.currentPage,
+                pageSize: $scope.bdpaginationConf.itemsPerPage
+            };
+            $scope.$watch('bdpaginationConf.currentPage + bdpaginationConf.itemsPerPage',policySearchDemand);
+        };
+        initPage();
+        $scope.policySearch=function () {
+            policySearchDemand();
+        };
 
 
     };
--- components/saledata/service/saledata.serv.js
+++ components/saledata/service/saledata.serv.js
@@ -4,7 +4,29 @@ define([], function () {
     'use strict';
     function saledataServHandler($http,$q) {
         return {
-
+            //查询
+            policySearchserv:function (conditionDto){
+            var deferred = $q.defer();
+            var promise  = $http({
+                method:'POST',
+                // method:'GET',
+                url:'data/policyData.json',
+                data:conditionDto
+            });
+            promise.then(
+                //通讯成功
+                function(answer){
+                    answer.status = true;
+                    deferred.resolve(answer);
+                },
+                //通讯失败
+                function(error){
+                    error.status = false;
+                    deferred.reject(error);
+                });
+            //返回promise对象，交由Controller继续处理
+            return deferred.promise;
+        },
         }
     };
 
