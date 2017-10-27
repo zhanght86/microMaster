6c8ee2ae78f4a62f9d1cf5d7a87119c6400d6d46
caoruixia <1361874583@qq.com>
2017-10-27 13:06:31 +0800

    add caoruixia  20171027  反馈管理添加查询分页功能

--- components/feedback/controller/feedback.ctrl.js
+++ components/feedback/controller/feedback.ctrl.js
@@ -2,9 +2,49 @@
  */
 define([], function () {
     'use strict';
-    function feedbackCtrlHandler ($scope,$state) {
+    function feedbackCtrlHandler ($scope,$state,feedbackServ) {
+        //----------------------------查询------------------------------------------
+        var feedSearchDemand=function () {
+            $scope.condition.pageNum= $scope.paginationConf.currentPage;
+            $scope.condition.pageSize=$scope.paginationConf.itemsPerPage;
+            var conditionDto = $scope.condition;
+            if(conditionDto == '' || conditionDto == null){
+            }
+            if($scope.roleName == undefined){
+                $scope.roleName = "";
+            }
+            conditionDto.roleName=$scope.roleName;
+            feedbackServ.feedSearchserv(conditionDto).then(
+                function(answer){
+                    // $scope.paginationConf.totalItems = answer.data.data.total;
+                    // $scope.dataDictionaryListR=answer.data.data.list;
+                    $scope.paginationConf.totalItems = answer.data.count;
+                    $scope.feedbackList=answer.data.items;
+                },function(error){
+                    //cconsole.log(JSON.stringify(error.data));
+                }
+            );
+        };
+        var initPage = function(){
+            $scope.paginationConf = {
+                currentPage: 1,
+                totalItems: 0,
+                itemsPerPage: 5,
+                pagesLength: 5,
+                perPageOptions: [5, 10, 15,20]
+            };
+            $scope.condition = {
+                pageNum: $scope.paginationConf.currentPage,
+                pageSize: $scope.paginationConf.itemsPerPage
+            };
+            $scope.$watch('paginationConf.currentPage + paginationConf.itemsPerPage',feedSearchDemand);
+        };
+        initPage();
+        $scope.feedSearch=function () {
+            feedSearchDemand();
+        };
         //查看详情
-        $scope.addsssssss=function () {
+        $scope.bigDescribe=function () {
             $scope.layer_detailText=true;
         };
         // 弹层关闭
--- components/feedback/service/feedback.serv.js
+++ components/feedback/service/feedback.serv.js
@@ -4,7 +4,29 @@ define([], function () {
     'use strict';
     function feedbackServHandler($http,$q){
         return {
-
+            //查询
+            feedSearchserv:function (conditionDto){
+            var deferred = $q.defer();
+            var promise  = $http({
+                method:'POST',
+                // method:'GET',
+                url:'data/feedbackData.json',//http://172.16.30.140:8090/gscore-pa-web
+                data:conditionDto
+            });
+            promise.then(
+                //通讯成功
+                function(answer){
+                    answer.status = true;
+                    deferred.resolve(answer);
+                },
+                //通讯失败
+                function(error){
+                    error.status = false;
+                    deferred.reject(error);
+                });
+            //返回promise对象，交由Controller继续处理
+            return deferred.promise;
+        },
 
 
 
