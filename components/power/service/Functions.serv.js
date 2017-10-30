910bb06c5239396ada6d387e2875a578655cea78
caoruixia <1361874583@qq.com>
2017-10-30 14:33:32 +0800

    add caoruixia  20171030 机构树研究，模拟数据测试

--- components/power/service/Functions.serv.js
+++ components/power/service/Functions.serv.js
@@ -5,7 +5,29 @@ define([], function () {
     function FunctionsServHandler($http,$q){
         return {
 
-
+            //机构树
+            getAptitTree:function (conditionDto){
+                var deferred = $q.defer();
+                var promise  = $http({
+                    method:'POST',
+                    // method:'GET',
+                    url:'data/FunctionsTreeData.json',//http://172.16.30.140:8090/gscore-pa-web
+                    data:conditionDto
+                });
+                promise.then(
+                    //通讯成功
+                    function(answer){
+                        answer.status = true;
+                        deferred.resolve(answer);
+                    },
+                    //通讯失败
+                    function(error){
+                        error.status = false;
+                        deferred.reject(error);
+                    });
+                //返回promise对象，交由Controller继续处理
+                return deferred.promise;
+            },
 
 
         }
